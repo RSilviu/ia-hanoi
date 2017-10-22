43ba25c1d5cf4b6ac10ba77dd4275e7244fb9c17
Silviu <1silviu.rusu@gmail.com>
2017-10-22 22:23:09 +0300

    adaugat alegere optimizata piesa

--- general.py
+++ general.py
@@ -1,4 +1,12 @@
 
+def piesa_urmatoare(SC,m,n):
+    i = 0
+    for j in range(2, m + 1):
+        if SC[j:] == [n] * (m - j + 1):
+            i = j
+            break
+    return i
+
 def tranzitie(SC,piesa,tija):
     SC[piesa] = tija
     return SC
@@ -8,7 +16,7 @@ def st_finala(SC,SF):
 
 def validare(SC,SA,piesa):
     SP = SA[1:piesa]
-    return not ((SC[piesa] in SP) or (SA[piesa] in SP))
+    return (not ((SC[piesa] in SP) or (SA[piesa] in SP)))
 
 
 # SI = (1,)*4
@@ -17,3 +25,14 @@ def validare(SC,SA,piesa):
 # l = [1]*4
 # print(l)
 # print(SI == tuple(l))
+
+# piesa 3 tija 3
+# 2 1 1
+# 2 1 3
+
+# 1 1 1
+# 1 1 3
+# 1 3 3
+# 3 3 3
+
+# 3 2 3 -> 3 3 3
